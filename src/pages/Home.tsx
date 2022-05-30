1c12b0726e8635a8e0f076f8dae24088db998557
FightM9 <fightm9@gmail.com>
2022-05-30 14:33:40 +0300

    ref: retun fecth data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,29 @@
 import { FC, useEffect, useState } from 'react';
 import ImageList from 'components/ImageList';
 import { useFavoriteCats } from 'hooks';
-import { fetchImages } from 'shared/api/config';
 import { Cat } from 'shared/types';
+import axios from 'axios';
+import { BASE } from 'shared/api/config';
 
 import 'shared/styles/button.scss';
 import 'shared/styles/page.scss';
+
+export const fetchImages = (page: number, limit = 20) =>
+  axios.get(`${BASE}/images/search?limit=${limit}&page=${page}&order=desc`);
 
 const Home: FC = () => {
   const [{ favoriteCats, toggleFavorite }] = useFavoriteCats();
   const [data, setData] = useState<Cat[]>([]);
   const [page, setPage] = useState(1);
-  const [isError, setIsError] = useState(1);
+  const [isError, setIsError] = useState(false);
 
   console.log('start home');
 
   useEffect(() => {
     fetchImages(page)
-    .then((result) => setData([...data, ...result.data]))
-    .catch();
+      .then((result) => setData([...data, ...result.data]))
+      .then(() => setIsError(false))
+      .catch(() => setIsError(true));
   }, [page]);
 
   useEffect(() => {
